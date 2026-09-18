0e6435b701a344e6e206d6bc19ee6b5f23dafe5c
AngelGonza1130Bice <angelgonzalez1120@gmail.com>
2026-09-18 02:23:37 +0000

    commit

---
+++ dia6.py
@@ -0,0 +1,20 @@
+# funciones the pyhton 
+"""
+# Como utilizat def Una función en Python es un bloque de código reutilizable que realiza una tarea específica.
+# se manda a llamar la funcion para que retorne un datos
+def my_function():
+    #Do this
+    #Then do this
+    #Finally do this
+    print("Hola, esta es mi primera funcion")
+
+# Llamada a la función para que se ejecute y muestre el mensaje
+my_function()   
+"""
+
+#practicando Ciclos for
+
+
+for i in range(1,21):
+    if i % 2 == 0:
+        print(i)
---
+++ hangman_proyect.py
@@ -0,0 +1,35 @@
+import random
+
+# lista de palabras
+lista_palabras = ["aardvark", "baboon", "camel", "clock", "mirror", "tornado", "crab", "library"]
+
+# selecciona una palabra aleatoria
+chosen_word = random.choice(lista_palabras)
+word_length = len(chosen_word)
+
+# imprime la palabra aleatoria
+#print(chosen_word)
+
+# crea los "_"
+placeholder = ""
+
+for e in range(word_length):
+    placeholder += "_"
+print(placeholder)
+
+# pregunta qué letra se adivina
+
+while guess != letter:
+    guess = input("Guess a letter: ").lower()
+
+# verifica si la letra se encuentra en la palabra
+    display = ""
+
+    for letter in chosen_word:
+    if letter == guess:
+        display += letter
+    else:
+        display += "_"
+
+# muestra el resultado
+print(display)
